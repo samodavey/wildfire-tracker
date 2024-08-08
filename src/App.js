5fd5ed31c18ab585f73b6c07107bf2cca5cd1837
Sam Davey <samkdavey@gmail.com>
2024-08-08 02:11:56 +0100

    Loader added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-
-import Map from './components/Map'
-
+import { useState, useEffect } from "react";
+import Map from "./components/Map";
+import Loader from "./components/Loader"
+//Use axios instead?
 function App() {
+  const [eventData, setEventData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true);
+      //Add to .env file?
+      const res = await fetch("https://eonet.gsfc.nasa.gov/api/v2.1/events");
+      const { events } = await res.json();
+
+      setEventData(events);
+      setLoading(false);
+    };
+
+    fetchEvents();
+  }, []);
+
   return (
     <div>
-      <Map/>
+      {!loading ? <Map eventData={eventData}/> : <Loader/>}
     </div>
   );
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,5 @@
 import GoogleMapReact from "google-map-react";
+import LocationMarker from "./LocationMarker";
 
 //Unable to restrict to env due to costs, hence visible API key
 // process.env.GOOGLE_MAPS_API_KEY 
@@ -7,10 +8,12 @@ const Map = ({ center, zoom }) => {
   return (
     <div className="map">
       <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyBQj1ThkyDR8Hr3aBOdb83vuUFIGV8r93A'}}
+        bootstrapURLKeys={{ key: 'AIzaSyAmhuIe8ndd8k8eAqTsIijr_IaSp3Zof_U'}}
         defaultCenter={center}
         defaultZoom={zoom}
-      />
+      >
+        <LocationMarker lat={center.lat} lng={center.lng}/>
+      </GoogleMapReact>
     </div>
   );
 };
